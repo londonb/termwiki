a1c308093d21bdf0599d57fb9617eac28e1a3a46
brad london and nic netzel <london.brad@gmail.com>
2016-01-14 11:24:57 -0800

    add lots of pages

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 $(document).ready(function() {
-  var terms = ["default" , "index"];
+  var terms = ["default" , "index", "array", "divspan", "css", "javascript-variables", "javascript-arithmetic", "javascript-methods", "forms"];
       terms.forEach(function(term) {
         $("ul.sidebarMenu").append('<li>'+ '<a href=' + '"'+ term + '.html">' + term + '</a></li>');
 
       });
+
+      $("#blanks form").submit(function(event){
+    var nameInput = $("input#yourName").val();
+    var questInput = $("input#yourQuest").val();
+    var colorInput = $("input#favColor").val();
+
+    $(".yourName").text(nameInput);
+    $(".yourQuest").text(questInput);
+    $("#colorized").css('color', colorInput);
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
 });
