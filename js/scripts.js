6d6f00528828ea31d1fd325ea2230a37d096d088
brad london and nic netzel <netzeln@gmail.com>
2016-01-14 09:05:49 -0800

    create initial template

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
+  var terms = ["default" , "index"];
+      terms.forEach(function(term) {
+        $("ul.sidebarMenu").append('<li>'+ '<a href=' + '"'+ term + '.html">' + term + '</a></li>');
 
-
-
+      });
 });
